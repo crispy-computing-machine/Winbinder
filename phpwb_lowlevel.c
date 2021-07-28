b129b31ef2a707d5f089cb882782cb84828dc984
Crispy Computing Machine <5684066+crispy-computing-machine@users.noreply.github.com>
2021-07-28 15:27:09 +0100

    FARPROC addr

--- phpwb_lowlevel.c
+++ phpwb_lowlevel.c
@@ -230,7 +230,8 @@ ZEND_FUNCTION(wb_get_function_address)
 {
 	char *fun;
 	int fun_len;
-	LONG_PTR addr, hlib = (LONG_PTR)NULL;
+	FARPROC addr
+	LONG_PTR hlib;
 
 	// low level functions disabled?
 	if (INI_INT("winbinder.low_level_functions") == 0)
